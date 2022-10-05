65c7e24148b5a1fb6e1a349bda141d5599a5de72
nely de faria <nelly.nf54@gmail.com>
2022-10-05 19:47:46 -0300

    Create variables.php

---
+++ basic-php/variables.php
@@ -0,0 +1,19 @@
+<?php
+
+// Variables
+// Is a symbol or name that stands for a value
+$name = 'Dary';
+$age = 24;
+
+echo "My name is {$name} <br>";
+echo "{$name} is {$age} years old <br>";
+
+/*
+    Variables names
+    Variables names starts with a letter A-Z a-z,
+    underscore (_), followed by any number of
+    letters, numbers, or underscores.
+*/
+
+$name = 'Taylor';
+echo $name;
