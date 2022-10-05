eada0b5da7840b4d00290a4adc218f995596351b
nely de faria <nelly.nf54@gmail.com>
2022-10-05 19:47:58 -0300

    Create variables-in.php

---
+++ basic-php/variables-in.php
@@ -0,0 +1,18 @@
+<?php
+
+// Add variables in echo/print
+$age = 24;
+/*
+    echo
+    Single quotes - variable named are echoed as it is
+    Double quotes - variable names are replaced by the
+    variable value
+*/
+
+// echo "My age is {$age}";
+// echo 'My age is ' . $age;
+
+// print $age;
+// print "My age is {$age}";
+// print 'My age is {$age}';
+print 'My age is ' . $age;
