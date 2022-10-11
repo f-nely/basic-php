3cb0b2c80e057bfa0258ed1db6beebba4d19bede
nely de faria <nelly.nf54@gmail.com>
2022-10-11 19:48:24 -0300

    Create multidimensional-arrays.php

---
+++ basic-php/multidimensional-arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+// Multidimensional arrays
+$cars = [
+    'Expensive' => ['Audi', 'Mercedes', 'BMW'], 
+    'Inexpensive' => ['Volvo', 'Ford', 'Toyota']
+];
+
+echo '<pre>';
+print_r($cars['Expensive']);
