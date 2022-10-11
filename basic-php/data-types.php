3ba1a0ee0382ece2cf4c41e8a9c6463e69d66789
nely de faria <nelly.nf54@gmail.com>
2022-10-11 19:47:14 -0300

    Create data-types.php

---
+++ basic-php/data-types.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+    Data types
+
+    integer
+    floating point or float
+    string
+    booleans
+    array
+    object
+    resource
+    null
+*/
+
+$age = 24;
+echo 'My age is ' . $age . '<br>';
+
+$price = 10.5;
+echo 'The price is $' . $price . '<br>';
+
+$name = 'Dary';
+echo 'My name is ' . $name . '<br>';
+
+$is_allowed = true;
+var_dump($is_allowed);
+
+$x = null;
