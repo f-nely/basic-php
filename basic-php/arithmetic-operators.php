846633dd35ca3a03915141a6b47e77f6fdea413e
nely de faria <nelly.nf54@gmail.com>
2022-10-13 13:54:48 -0300

    Create arithmetic-operators.php

---
+++ basic-php/arithmetic-operators.php
@@ -0,0 +1,14 @@
+<?php
+
+// Arithmetic operators
+// + - * / %
+
+$x = 5;
+$y = 3;
+
+// echo $x + 5;
+// echo $x - $y;
+// echo round($x / $y, 2);
+// echo $x * $y;
+// echo $x ** $y;
+echo $x % $y;
