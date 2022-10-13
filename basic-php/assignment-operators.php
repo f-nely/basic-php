8e1d06f204ee132af911bba80d4d45bfe617798b
nely de faria <nelly.nf54@gmail.com>
2022-10-13 13:55:07 -0300

    Create assignment-operators.php

---
+++ basic-php/assignment-operators.php
@@ -0,0 +1,22 @@
+<?php
+
+// Assignment operators
+/*
+    += -- Add and assign
+    -= -- Subtract and assign
+    *= -- Multiply and assign
+    /= -- Divide and assign
+    .= -- Conatenate ad assign
+*/
+
+$x = 5;
+// $x += 5;
+// $x -= 5;
+// $x *= 5;
+// $x /= 5;
+
+// echo $x;
+
+$str = 'Dary is a old man';
+$str .= ' because he is 24 years old!';
+echo $str;
