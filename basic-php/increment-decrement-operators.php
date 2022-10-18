064a502a841785296c72efd2afc946a0ca21197e
nely de faria <nelly.nf54@gmail.com>
2022-10-18 20:05:52 -0300

    Create increment-decrement-operators.php

---
+++ basic-php/increment-decrement-operators.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+    Increment and decrement operators
+
+    ++$x -- Pre-increment
+    $x++ -- Post-increment
+    --$x -- Pre-decrement
+    $x-- -- Post-decrement
+*/
+
+$x = 10;
+
+// echo ++$x;
+// $x++;
+// echo $x;
+echo --$x;
