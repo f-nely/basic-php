cd458409a7055de004be54f12638936aa7ebf407
nely de faria <nelly.nf54@gmail.com>
2022-11-17 16:59:30 -0300

    Create first-function.php

---
+++ basic-php/first-function.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+    Functions
+
+    A function is a block of code written in a program to
+    perform some specific task.
+*/
+
+function myFunction(): string
+{
+    return 'Hello!';
+}
+
+echo myFunction();
