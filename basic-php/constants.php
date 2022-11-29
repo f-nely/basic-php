42345be6a0f62b828d5048caab5b2e290617cd6d
nely de faria <nelly.nf54@gmail.com>
2022-11-29 11:32:37 -0300

    Create constants.php

---
+++ basic-php/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+    Constants
+
+    * Function - define()
+    * Keyword - const
+*/
+
+// Define
+// Constants are always in uppercase
+// Name has the same rules as variables
+define('COMPANY_NAME', 'Apple');
+echo COMPANY_NAME;
+echo '<br>';
+
+// Const
+const MY_NAME = 'Dary';
+echo MY_NAME;
