b04f9eba39f55ccfbb5afa5df1c9fa66d8240500
nely de faria <nelly.nf54@gmail.com>
2022-12-01 12:36:30 -0300

    Create math-build-in-functions.php

---
+++ basic-php/math-build-in-functions.php
@@ -0,0 +1,18 @@
+<?php
+
+// Math functions
+
+// ceil
+echo ceil(1.5);
+echo '<br>';
+
+// floor
+echo floor(1.5);
+echo '<br>';
+
+// round
+echo round(0.54335, 4);
+echo '<br>';
+
+// Calculates the exponent of e
+echo exp(4);
