d9ae41889a1a52ffb4293d3401a5542aaa724232
nely de faria <nelly.nf54@gmail.com>
2022-12-01 12:37:45 -0300

    Create date-time-functions.php

---
+++ basic-php/date-time-functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+    Date
+    d = day
+    j = day without zeros
+    D = day of week (3 letters)
+    l = full day of the week
+    m = month as a number with zeros
+    n = month as a number without zeros
+    M = month (3 letters)
+    F = full month
+    y = two-digit year
+    Y = full year
+*/
+
+// echo date('d/m/Y');
+// echo date('D');
+
+/*
+    Time
+    g = hours in 12-hour format without zeros
+    h = hours in 12-hour format with zeros
+    G = hours in 24-hour format without zeros
+    H = hours in 24-hour format with zeros
+    a = am/pm in lowercase
+    A = am/pm in uppercase
+    i = minutes without leading zeros
+    s = seconds without leading zeros
+*/
+
+echo date('H:i:s');
