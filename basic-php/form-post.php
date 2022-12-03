d4bfc5dde266d7b2f303c7dde8e63a97f725c01a
nely de faria <nelly.nf54@gmail.com>
2022-12-03 15:46:16 -0300

    Create form-post.php

---
+++ basic-php/form-post.php
@@ -0,0 +1,14 @@
+<form action="post.php" method="POST">
+    <fieldset>
+        <legend>Personal Data</legend>
+        <div>
+            <input type="text" name="name" id="name" placeholder="--- Your name ---">
+        </div><br>
+        <div>
+            <input type="text" name="age" id="age" placeholder="--- Your age ---">
+        </div><br>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </fieldset>
+</form>
